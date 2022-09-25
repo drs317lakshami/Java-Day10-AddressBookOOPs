1dacd2afd750d818c782bc7211c0b228a09d0405
drs317lakshami <devrajkumar317@gmail.com>
2022-09-25 16:19:41 +0530

    Edit contact details

---
+++ Contacts.java
@@ -0,0 +1,55 @@
+public class Contacts {
+	
+	
+		public String firstN, lastN, address, city, state, email, zip, phN;
+		
+		public String getFirstN() {
+			return firstN;
+		}
+		public void setFirstN(String firstN) {
+			this.firstN = firstN;
+		}
+		public String getLastN() {
+			return lastN;
+		}
+		public void setLastN(String lastN) {
+			this.lastN = lastN;
+		}
+		public String getAddress() {
+			return address;
+		}
+		public void setAddress(String address) {
+			this.address = address;
+		}
+		public String getCity() {
+			return city;
+		}
+		public void setCity(String city) {
+			this.city = city;
+		}
+		public String getState() {
+			return state;
+		}
+		public void setState(String state) {
+			this.state = state;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public String getZip() {
+			return zip;
+		}
+		public void setZip(String zip) {
+			this.zip = zip;
+		}
+		public String getPhN() {
+			return phN;
+		}
+		public void setPhN(String phN) {
+			this.phN = phN;
+		}
+		
+	}
