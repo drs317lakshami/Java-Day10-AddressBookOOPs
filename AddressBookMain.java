e847bc78e8237489ed12e6f8a2e6eca480e80a36
drs317lakshami <devrajkumar317@gmail.com>
2022-09-25 16:36:14 +0530

    Edit contact details

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,30 +1,40 @@
 package com.blz.addressbook.oops;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBookMain {
 	
 	public static void main(String[] args) {
 		
+		ArrayList<Contacts> contactList = new ArrayList<>();
 		System.out.println("*****Welcome to My Address Book*****");
-		
 		Character ch = 'a';
 		Scanner sc = new Scanner(System.in);
+		AddressBookServices con = new AddressBookServImpl();
 		
 		while(!ch.equals('q')) {
 			
-			 System.out.print("\na) Add \nq) Quit \nEnter a character: ");
+			 System.out.print("\n a) Add \n e) Edit \n q) Quit \nEnter a character: ");
 		      ch = sc.next().charAt(0);
 		      
 		      switch (ch) {
 		        case 'a':
-		        	AddressBookServices con = new AddressBookServImpl();
-		            con.createContact();
+		        	Contacts c = con.createContact();
+		        	contactList.add(c);
+		        	con.displayContact(c);
 		            System.out.println("Added person's details : ");
-		            con.displayContact();
-		          break;
+		            
+		            break;
+		          
+		        case 'e' :
+		        	 Contacts c2 = con.createContact();
+		        	 con.editContact(contactList);
+		        	 System.out.println("Contact has been edited");
+		        	 con.displayContact(c2);
+		        	 break;
 
 		        case 'q':
-		          break;
+		        	 break;
 
 		        default:
 		          System.out.println("Please enter a valid character");
@@ -32,6 +42,98 @@ public class AddressBookMain {
 
 		      }
 		}
+		
+		sc.close();
+	}
+
+}
+
+interface AddressBookServices {
+	
+	public Contacts createContact();
+	public void displayContact(Contacts con);
+	public void editContact(ArrayList<Contacts> contactList);
+	
+}
+
+class AddressBookServImpl implements AddressBookServices {
+	
+	Scanner sc = new Scanner(System.in);
+	
+	@Override
+	public Contacts createContact() {
+		
+		Contacts con = new Contacts();
+	
+		System.out.println("Enter First Name : ");
+		con.setFirstN(sc.nextLine());
+		
+		System.out.println("Enter Last Name : ");
+		con.setLastN(sc.nextLine());
+		
+		System.out.println("Address : ");
+		con.setAddress(sc.nextLine());
+		
+		System.out.println("City : ");
+		con.setCity(sc.nextLine());
+		
+		System.out.println("State : ");
+		con.setState(sc.nextLine());
+		
+		System.out.println("E-mail : ");
+		con.setEmail(sc.nextLine());
+		
+		return con;
+	}
+	
+	@Override
+	public void displayContact(Contacts con) {
+		
+		System.out.println("First Name : " + con.getFirstN());
+		System.out.println("Last Name : " + con.getLastN());
+		System.out.println("Address : " + con.getAddress());
+		System.out.println("City : " + con.getCity());
+		System.out.println("State : " + con.getState());
+		System.out.println("E-mail : " + con.getEmail());
+
+	}
+
+	@Override
+	public void editContact(ArrayList<Contacts> contactList) {
+		if(contactList.size() == 0)
+		{
+			System.out.println("First add a name");
+			return;
+		}
+		System.out.print("Your existing contacts are : " );
+		for(Contacts c: contactList) {
+			System.out.print(c.getFirstN() + " ");
+		}
+		
+		System.out.print("Enter name of contact you wanna edit : ");
+		String firstN = sc.nextLine(); 
+		
+		int index = checkContact(contactList, firstN);
+		if(index == -1)
+		{
+			System.out.println("name not found");
+		
+		}else {
+			
+			contactList.set(index, createContact());
+		}
+	      }
+
+	private int checkContact(ArrayList<Contacts> contactList, String name) {
+		for(int i = 0; i < contactList.size(); i++) {
+			if(contactList.get(i).firstN.equals(name))
+			return i;
+		}
+		
+		return -1;
+	}
+		
+}
 		
 		sc.close();
 	}
